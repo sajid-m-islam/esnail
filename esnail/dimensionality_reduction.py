f219287c6ecb40044f4d6ebb59427e1e2f407aef
Sajid Islam <sajnyc360@gmail.com>
2026-05-14 21:49:23 -0400

    reduced features down to 40 and saved into new dataset

--- esnail/dimensionality_reduction.py
+++ esnail/dimensionality_reduction.py
@@ -13,7 +13,7 @@ df["Electrical Resistivity"] = pd.to_numeric(df["Electrical Resistivity"], error
 # Drop any columns with nan
 df = df.dropna(subset=["Seebeck Coefficient", "Electrical Resistivity"]).reset_index(drop=True)
 
-# Assign taget and features, drop all non-numerical data
+# Assign target and features, drop all non-numerical data
 target = "Seebeck Coefficient"
 cols_to_drop = ["Composition", "Site_X", "Site_Y", "Site_Z", "Seebeck Coefficient", "Electrical Resistivity"]
 
@@ -39,9 +39,27 @@ feature_importance_df = pd.DataFrame({
     "Importance": importances,
 }).sort_values(by="Importance", ascending=False).reset_index(drop=True)
 
+# Display top 20 features
 plt.figure(figsize=(10, 8))
 plt.barh(feature_importance_df["Feature"][:20][::-1], feature_importance_df["Importance"][:20][::-1])
 plt.xlabel("RandomForest Importance Score")
 plt.title(f"Top 20 Most Important Features for {target}")
 plt.tight_layout()
 plt.show()
+
+# Get top 40 features
+top_features = feature_importance_df["Feature"][:40].tolist()
+
+# Create new dataframe with reduced features and save in csv
+final_cols = ["Composition", "Site_X", "Site_Y", "Site_Z"] + top_features + ["Seebeck Coefficient", "Electrical Resistivity"]
+
+df_reduced = df[final_cols]
+df_reduced.to_csv("heusler_reduced_features.csv")
+
+print("\n" + "="*40)
+print(f"Dimensionality Reduction Complete")
+print(f"Reduced from {X.shape[1]} features down to 40.")
+print("="*40)
+    
+print("\nTop 5 absolute best predictors:")
+print(feature_importance_df.head(5))
